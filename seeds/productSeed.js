96abc3d9a7599f0c8931850ab5bfcb3f594c4e6d
DallasThompson <duhtennis1@gmail.com>
2023-10-31 11:50:21 -0800

    Added keyboards to productSeed.js

--- seeds/productSeed.js
+++ seeds/productSeed.js
@@ -4,7 +4,7 @@ const productData = [
 		price: 449.99,
 		image:
 			"https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/510830/510830_o01_111020/510830",
-		rating: 4,
+
 		short_desc: "Stay cool on this WorkPro Quantum",
 		long_desc:
 			"The adjustable office chair's mesh back and seat help promote airflow to keep you comfortable when answering e-mails, meeting with co-workers and handling day-to-day duties.",
@@ -15,7 +15,7 @@ const productData = [
 		price: 199.99,
 		image:
 			"https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/9579648/9579648_o01_052322/9579648",
-		rating: 4,
+
 		short_desc: "A breathable mesh seat and back help you stay cool.",
 		long_desc:
 			"The lumbar support can be adjusted to your ideal height for optimum positioning. Utilize tilt, recline and seat height adjustments to find your perfect position during the workday or at lunchtime.",
@@ -26,7 +26,7 @@ const productData = [
 		price: 309.99,
 		image:
 			"https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/9003237/9003237_o01_051923/9003237",
-		rating: 4,
+
 		short_desc: "It is about balance in the workplace nowadays",
 		long_desc:
 			"With a perfect mix of comfort and performance, this Serta office chair features 5 layers of foam with individually wrapped ComfortCoils™ for pressure point relief and temperature control during extended use.",
@@ -37,7 +37,7 @@ const productData = [
 		price: 131.99,
 		image:
 			"https://media.officedepot.com/images/f_auto,q_auto,e_sharpen/products/761468/761468_o01_091321/761468",
-		rating: 4,
+
 		short_desc: "Revamp your office space with a ventilated mesh office chair ",
 		long_desc:
 			"Contemporary Task Office Chair, Mid-Back Design, and Ventilated Decorative Mesh Back will make a great addition in modern and industrial spaces.",
@@ -48,7 +48,7 @@ const productData = [
 		price: 649.99,
 		image:
 			"https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/3497373/3497373_o01_101422/3497373",
-		rating: ,
+
 		short_desc: "Choose this Dell Inspiron 15 3520 laptop to get things done.",
 		long_desc:
 			" The 15.6 laptop screen provides FHD resolution and helps prevent glare. The Windows 11 laptop comes with a helpful operating system, offering fun features right away.",
@@ -59,7 +59,7 @@ const productData = [
 		price: 649.99,
 		image:
 			"https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/3497373/3497373_o01_101422/3497373",
-		rating: 4,
+
 		short_desc: "Choose this Dell Inspiron 15 3520 laptop to get things done.",
 		long_desc:
 			"The 15.6 laptop screen provides FHD resolution and helps prevent glare. The Windows 11 laptop comes with a helpful operating system, offering fun features right away.",
@@ -70,7 +70,7 @@ const productData = [
 		price: 929.99,
 		image:
 			"https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/8856075/8856075_o01/8856075",
-		rating: 4,
+
 		short_desc: "With the Dell Inspiron laptop, you can surf, stream, game and work from 1 device.",
 		long_desc:
 			"The Windows 11 laptop features a user-friendly operating system to help you get started. The 16 inch laptop display offers crystal-clear picture quality, and the FHD camera lets you take photos, record video or chat with friends and family face-to-face.",
@@ -81,54 +81,52 @@ const productData = [
 		price: 279.99,
 		image:
 			"https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/8896386/8896386",
-		rating: 0,
+
 		short_desc: "Manage your workflow or game with friends using this Dell Latitude PC.",
 		long_desc:
 			"The Windows 10 laptop offers wireless technology and a built-in webcam to help keep you connected. The 13.3 inch display offers touch-screen functionality, helping you control the refurbished laptop with a tap, pinch or swipe.",
 		category_id: 2,
 	},
     {
-		name: "",
-		price:,
+		name: "Logitech® K270 Wireless Keyboard",
+		price: 34.99,
 		image:
-			"",
-		rating: ,
-		short_desc: "",
+			"https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/610663/610663_p/610663",
+
+		short_desc: "Enhance your typing accuracy with this Logitech wireless keyboard.",
 		long_desc:
-			"",
+			" The 2.4GHz RF technology supports wireless connectivity, while 8 hot keys let you program your device for effortless operation. A UV coating keeps off harmful sun rays for durability. This Logitech wireless keyboard uses 2 AAA batteries for flawless operation in off-grid locations, and the spill-resistant design stands up to minor accidental liquid splashes.",
 		category_id: 3,
 	},
     {
-		name: "",
-		price:,
+		name: "Logitech® K780 Multi-Device Wireless Keyboard",
+		price: 64.99,
 		image:
-			"",
-		rating: ,
-		short_desc: "",
+			"https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/716162/716162_o01_122021/716162",
+
+		short_desc: "Enjoy exceptional typing on your computer",
 		long_desc:
-			"",
+			"All from one fully equipped and beautifully finished keyboard. The K780 Multi-Device redefines what a desktop keyboard can do, letting you switch typing easily between all the devices you enter text on.",
 		category_id: 3,
 	},
     {
-		name: "",
-		price:,
+		name: "Logitech K380 Multi-Device Bluetooth Wireless Keyboard",
+		price: 29.99,
 		image:
-			"",
-		rating: ,
-		short_desc: "",
+			"https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/212351/212351_o01_112921/212351",
+		short_desc: "The Logitech® K380 Multi-Device Bluetooth® keyboard makes any space minimalist.",
 		long_desc:
-			"",
+			"Connect with up to three Bluetooth-enabled devices simultaneously and switch instantly between them.The K380 Multi-Device is compact and lightweight.",
 		category_id: 3,
 	},
     {
-		name: "",
-		price:,
+		name: "AbleNet Keys-U-See Large print keyboards",
+		price: 45.69,
 		image:
-			"",
-		rating: ,
-		short_desc: "",
+			"https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/5161860/5161860_o51/5161860",
+		short_desc: "Keys-U-See large print keyboards are the perfect replacement.",
 		long_desc:
-			"",
+			"For anyone who has a hard time seeing the letters on their computer keyboard. The large print keyboards are also great for anyone working in low light conditions or are learning to type.",
 		category_id: 3,
 	},
     {
