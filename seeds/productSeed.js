520704800b524b4fce837a73760ca1280fa75a85
DallasThompson <duhtennis1@gmail.com>
2023-10-31 11:06:25 -0800

    added chair seed data

---
+++ seeds/productSeed.js
@@ -0,0 +1,47 @@
+const productData = [
+	{
+		name: "WorkPro® Quantum 9000 Series Ergonomic Mesh/Mesh Mid-Back Chair, Black/Black, BIFMA Compliant",
+		price: 449.99,
+		image:
+			"https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/510830/510830_o01_111020/510830",
+		rating: 4,
+		short_desc: "Stay cool on this WorkPro Quantum",
+		long_desc:
+			"The adjustable office chair's mesh back and seat help promote airflow to keep you comfortable when answering e-mails, meeting with co-workers and handling day-to-day duties.",
+		category_id: 1,
+	},
+    {
+		name: "WorkPro® Sentrix Ergonomic Mesh/Mesh Mid-Back Manager Chair, Fixed Arms, Black, BIFMA Compliant",
+		price: 199.99,
+		image:
+			"https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/9579648/9579648_o01_052322/9579648",
+		rating: 4,
+		short_desc: "A breathable mesh seat and back help you stay cool.",
+		long_desc:
+			"The lumbar support can be adjusted to your ideal height for optimum positioning. Utilize tilt, recline and seat height adjustments to find your perfect position during the workday or at lunchtime.",
+		category_id: 1,
+	},
+    {
+		name: "Serta® Smart Layers™ Brinkley Ergonomic Bonded Leather High-Back Executive Chair, Black/Silver",
+		price: 309.99,
+		image:
+			"https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/9003237/9003237_o01_051923/9003237",
+		rating: 4,
+		short_desc: "It is about balance in the workplace nowadays",
+		long_desc:
+			"With a perfect mix of comfort and performance, this Serta office chair features 5 layers of foam with individually wrapped ComfortCoils™ for pressure point relief and temperature control during extended use.",
+		category_id: 1,
+	},
+    {
+		name: "Flash Furniture Mesh Mid-Back Swivel Task Chair With Padded Seat, Black",
+		price: 131.99,
+		image:
+			"https://media.officedepot.com/images/f_auto,q_auto,e_sharpen/products/761468/761468_o01_091321/761468",
+		rating: 4,
+		short_desc: "Revamp your office space with a ventilated mesh office chair ",
+		long_desc:
+			"Contemporary Task Office Chair, Mid-Back Design, and Ventilated Decorative Mesh Back will make a great addition in modern and industrial spaces.",
+		category_id: 1,
+	},
+    
+];
