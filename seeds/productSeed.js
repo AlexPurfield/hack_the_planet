d8df576daa327402dcd6dd8cd3b694c88b650ab8
DallasThompson <duhtennis1@gmail.com>
2023-10-31 11:31:34 -0800

    added laptop product data

--- seeds/productSeed.js
+++ seeds/productSeed.js
@@ -43,5 +43,180 @@ const productData = [
 			"Contemporary Task Office Chair, Mid-Back Design, and Ventilated Decorative Mesh Back will make a great addition in modern and industrial spaces.",
 		category_id: 1,
 	},
-    
+    {
+		name: "Dell™ Inspiron 15 3520 Laptop",
+		price: 649.99,
+		image:
+			"https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/3497373/3497373_o01_101422/3497373",
+		rating: ,
+		short_desc: "Choose this Dell Inspiron 15 3520 laptop to get things done.",
+		long_desc:
+			" The 15.6 laptop screen provides FHD resolution and helps prevent glare. The Windows 11 laptop comes with a helpful operating system, offering fun features right away.",
+		category_id: 2,
+	},
+    {
+		name: "Dell™ Inspiron 15 3520 Laptop",
+		price: 649.99,
+		image:
+			"https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/3497373/3497373_o01_101422/3497373",
+		rating: 4,
+		short_desc: "Choose this Dell Inspiron 15 3520 laptop to get things done.",
+		long_desc:
+			"The 15.6 laptop screen provides FHD resolution and helps prevent glare. The Windows 11 laptop comes with a helpful operating system, offering fun features right away.",
+		category_id: 2,
+	},
+    {
+		name: "Dell™ Inspiron 16 5630 Laptop",
+		price: 929.99,
+		image:
+			"https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/8856075/8856075_o01/8856075",
+		rating: 4,
+		short_desc: "With the Dell Inspiron laptop, you can surf, stream, game and work from 1 device.",
+		long_desc:
+			"The Windows 11 laptop features a user-friendly operating system to help you get started. The 16 inch laptop display offers crystal-clear picture quality, and the FHD camera lets you take photos, record video or chat with friends and family face-to-face.",
+		category_id: 2,
+	},
+    {
+		name: "Dell™ Latitude 7390 2-in-1 Refurbished Laptop",
+		price: 279.99,
+		image:
+			"https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/8896386/8896386",
+		rating: 0,
+		short_desc: "Manage your workflow or game with friends using this Dell Latitude PC.",
+		long_desc:
+			"The Windows 10 laptop offers wireless technology and a built-in webcam to help keep you connected. The 13.3 inch display offers touch-screen functionality, helping you control the refurbished laptop with a tap, pinch or swipe.",
+		category_id: 2,
+	},
+    {
+		name: "",
+		price:,
+		image:
+			"",
+		rating: ,
+		short_desc: "",
+		long_desc:
+			"",
+		category_id: 3,
+	},
+    {
+		name: "",
+		price:,
+		image:
+			"",
+		rating: ,
+		short_desc: "",
+		long_desc:
+			"",
+		category_id: 3,
+	},
+    {
+		name: "",
+		price:,
+		image:
+			"",
+		rating: ,
+		short_desc: "",
+		long_desc:
+			"",
+		category_id: 3,
+	},
+    {
+		name: "",
+		price:,
+		image:
+			"",
+		rating: ,
+		short_desc: "",
+		long_desc:
+			"",
+		category_id: 3,
+	},
+    {
+		name: "",
+		price:,
+		image:
+			"",
+		rating: ,
+		short_desc: "",
+		long_desc:
+			"",
+		category_id: 4,
+	},
+    {
+		name: "",
+		price:,
+		image:
+			"",
+		rating: ,
+		short_desc: "",
+		long_desc:
+			"",
+		category_id: 4,
+	},
+    {
+		name: "",
+		price:,
+		image:
+			"",
+		rating: ,
+		short_desc: "",
+		long_desc:
+			"",
+		category_id: 4,
+	},
+    {
+		name: "",
+		price:,
+		image:
+			"",
+		rating: ,
+		short_desc: "",
+		long_desc:
+			"",
+		category_id: 4,
+	},
+    {
+		name: "",
+		price:,
+		image:
+			"",
+		rating: ,
+		short_desc: "",
+		long_desc:
+			"",
+		category_id: 5,
+	},
+    {
+		name: "",
+		price:,
+		image:
+			"",
+		rating: ,
+		short_desc: "",
+		long_desc:
+			"",
+		category_id: 5,
+	},
+    {
+		name: "",
+		price:,
+		image:
+			"",
+		rating: ,
+		short_desc: "",
+		long_desc:
+			"",
+		category_id: 5,
+	},
+    {
+		name: "",
+		price:,
+		image:
+			"",
+		rating: ,
+		short_desc: "",
+		long_desc:
+			"",
+		category_id: 5,
+	},
 ];
