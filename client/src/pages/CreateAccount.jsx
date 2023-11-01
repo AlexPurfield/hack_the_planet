7ee5010a2fd68f9851523f1ee6395a3750e33de6
Steven Rodriguez <54337759+StevenRodriguezDev@users.noreply.github.com>
2023-11-01 12:16:04 -0600

    update create account

--- client/src/pages/CreateAccount.jsx
+++ client/src/pages/CreateAccount.jsx
@@ -32,98 +32,45 @@ function CreateAccount(props) {
     };
   
     return (
-    //   <div className="container my-1">
-    //     <Link to="/login">← Go to Login</Link>
+      <div className="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5" tabIndex="-1" role="dialog" id="modalSignin">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content rounded-4 shadow">
+            <div className="modal-header p-5 pb-4 border-bottom-0">
+              <h1 className="fw-bold mb-0 fs-2">Sign up for free</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
   
-    //     <h2>CreateAccount</h2>
-    //     <form onSubmit={handleFormSubmit}>
-    //       <div className="flex-row space-between my-2">
-    //         <label htmlFor="firstName">First Name:</label>
-    //         <input
-    //           placeholder="First"
-    //           name="firstName"
-    //           type="firstName"
-    //           id="firstName"
-    //           onChange={handleChange}
-    //         />
-    //       </div>
-    //       <div className="flex-row space-between my-2">
-    //         <label htmlFor="lastName">Last Name:</label>
-    //         <input
-    //           placeholder="Last"
-    //           name="lastName"
-    //           type="lastName"
-    //           id="lastName"
-    //           onChange={handleChange}
-    //         />
-    //       </div>
-    //       <div className="flex-row space-between my-2">
-    //         <label htmlFor="email">Email:</label>
-    //         <input
-    //           placeholder="youremail@test.com"
-    //           name="email"
-    //           type="email"
-    //           id="email"
-    //           onChange={handleChange}
-    //         />
-    //       </div>
-    //       <div className="flex-row space-between my-2">
-    //         <label htmlFor="pwd">Password:</label>
-    //         <input
-    //           placeholder="******"
-    //           name="password"
-    //           type="password"
-    //           id="pwd"
-    //           onChange={handleChange}
-    //         />
-    //       </div>
-    //       <div className="flex-row flex-end">
-    //         <button type="submit">Submit</button>
-    //       </div>
-    //     </form>
-    //   </div>
-
-  <div class="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5" tabindex="-1" role="dialog" id="modalSignin">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content rounded-4 shadow">
-      <div class="modal-header p-5 pb-4 border-bottom-0">
-        <h1 class="fw-bold mb-0 fs-2">Sign up for free</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-
-      <div class="modal-body p-5 pt-0">
-        <form class="">
-          <div class="form-floating mb-3">
-            <input type="email" class="form-control rounded-3" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Email address</label>
+            <div className="modal-body p-5 pt-0">
+              <form onSubmit={handleFormSubmit}>
+                <div className="form-floating mb-3">
+                  <input type="email" className="form-control rounded-3" id="floatingInput" placeholder="name@example.com" name="email" value={formState.email} onChange={handleChange} />
+                  <label htmlFor="floatingInput">Email address</label>
+                </div>
+                <div className="form-floating mb-3">
+                  <input type="password" className="form-control rounded-3" id="floatingPassword" placeholder="Password" name="password" value={formState.password} onChange={handleChange} />
+                  <label htmlFor="floatingPassword">Password</label>
+                </div>
+                <button className="w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit">Sign up</button>
+                <small className="text-body-secondary">By clicking Sign up, you agree to the terms of use.</small>
+                <hr className="my-4" />
+                <h2 className="fs-5 fw-bold mb-3">Or use a third-party</h2>
+                <button className="w-100 py-2 mb-2 btn btn-outline-secondary rounded-3" type="submit">
+                  <svg className="bi me-1" width="16" height="16"><use xlinkHref="#twitter"/></svg>
+                  Sign up with Twitter
+                </button>
+                <button className="w-100 py-2 mb-2 btn btn-outline-primary rounded-3" type="submit">
+                  <svg className="bi me-1" width="16" height="16"><use xlinkHref="#facebook"/></svg>
+                  Sign up with Facebook
+                </button>
+                <button className="w-100 py-2 mb-2 btn btn-outline-secondary rounded-3" type="submit">
+                  <svg className="bi me-1" width="16" height="16"><use xlinkHref="#github"/></svg>
+                  Sign up with GitHub
+                </button>
+              </form>
+            </div>
           </div>
-          <div class="form-floating mb-3">
-            <input type="password" class="form-control rounded-3" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Password</label>
-          </div>
-          <button class="w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit">Sign up</button>
-          <small class="text-body-secondary">By clicking Sign up, you agree to the terms of use.</small>
-          <hr class="my-4">
-          <h2 class="fs-5 fw-bold mb-3">Or use a third-party</h2>
-          <button class="w-100 py-2 mb-2 btn btn-outline-secondary rounded-3" type="submit">
-            <svg class="bi me-1" width="16" height="16"><use xlink:href="#twitter"/></svg>
-            Sign up with Twitter
-          </button>
-          <button class="w-100 py-2 mb-2 btn btn-outline-primary rounded-3" type="submit">
-            <svg class="bi me-1" width="16" height="16"><use xlink:href="#facebook"/></svg>
-            Sign up with Facebook
-          </button>
-          <button class="w-100 py-2 mb-2 btn btn-outline-secondary rounded-3" type="submit">
-            <svg class="bi me-1" width="16" height="16"><use xlink:href="#github"/></svg>
-            Sign up with GitHub
-          </button>
-        </form>
+        </div>
       </div>
-    </div>
-  </div>
-</div>
-
-    
-);
+    );
 }
 export default CreateAccount;
