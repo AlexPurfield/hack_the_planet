3ac305ab51f9ab63003036809fc4199aaa87e331
Alex Purfield <alexpurfield@gmail.com>
2023-11-01 13:08:50 -0600

    fixed seeds bugs

--- server/config/cleanDB.js
+++ server/config/cleanDB.js
@@ -3,7 +3,7 @@ const db = require('../config/connection');
 
 module.exports = async (modelName, collectionName) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
+    let modelExists = await models.Product.db.db.listCollections({
       name: collectionName
     }).toArray()
 
