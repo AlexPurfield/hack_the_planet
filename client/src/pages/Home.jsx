3a9cf7a198e0f44d3d9662507ff0baf7e0fa8b96
Steven Rodriguez <54337759+StevenRodriguezDev@users.noreply.github.com>
2023-11-02 10:18:55 -0600

    updated home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,11 @@
+// import CreateAccount from "./CreateAccount";
+// import Login from "./Login";
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-    
+      {/* <CreateAccount /> */}
+      {/* <Login /> */}
     </div>
   );
 };
