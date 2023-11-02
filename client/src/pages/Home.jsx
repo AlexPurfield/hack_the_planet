ddc1df15b6e2a68503ba15f415e1e75ab0fad06b
Steven Rodriguez <54337759+StevenRodriguezDev@users.noreply.github.com>
2023-11-02 14:10:06 -0600

    updated home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,10 @@
 import HomeCarousel from "../components/HomeCarousel/HomeCarousel";
 
-// import CreateAccount from "./CreateAccount";
-// import Login from "./Login";
 const Home = () => {
   return (
     <div>
       <HomeCarousel />
       <h1>Home</h1>
-      {/* <CreateAccount /> */}
-      {/* <Login /> */}
     </div>
   );
 };
