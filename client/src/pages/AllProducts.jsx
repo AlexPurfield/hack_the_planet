573960edb00f56bd2afe7f8d48d9b7af59e21773
DallasThompson <duhtennis1@gmail.com>
2023-11-03 11:06:10 -0800

    Finishing up the products page.

--- client/src/pages/AllProducts.jsx
+++ client/src/pages/AllProducts.jsx
@@ -5,161 +5,7 @@ import { Row, Col, Container } from "react-bootstrap";
 import ProductCard from "../components/ProductCard/ProductCard";
 
 const AllProducts = () => {
-<<<<<<< HEAD
-  
-//   const [products, setProducts] = useState([	{
-//     name: "",
-//     price: 0,
-//     image:
-//         "",
-//     short_desc: "",
-// },]);
-const products = [
-    {
-      _id: 1,
-      name: "LAPTOP",
-      description: "hacker pick",
-      price: 999.99,
-      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhF1MePWubfQaH5IsEoOcJFrninOPR_cF4iw&usqp=CAU",
-    },
-    {
-      _id: 2,
-      name: "LAPTOP",
-      description: "hacker pick",
-      price: 999.99,
-      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhF1MePWubfQaH5IsEoOcJFrninOPR_cF4iw&usqp=CAU",
-    },
-    {
-      _id: 3,
-      name: "LAPTOP",
-      description: "hacker pick",
-      price: 999.99,
-      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhF1MePWubfQaH5IsEoOcJFrninOPR_cF4iw&usqp=CAU",
-    },
-    {
-      _id: 4,
-      name: "LAPTOP",
-      description: "hacker pick",
-      price: 999.99,
-      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhF1MePWubfQaH5IsEoOcJFrninOPR_cF4iw&usqp=CAU",
-    },
-    {
-      _id: 5,
-      name: "KEYBOARD",
-      description: "hacker pick",
-      price: 999.99,
-      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhF1MePWubfQaH5IsEoOcJFrninOPR_cF4iw&usqp=CAU",
-    },
-    {
-      _id: 6,
-      name: "KEYBOARD",
-      description: "hacker pick",
-      price: 999.99,
-      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhF1MePWubfQaH5IsEoOcJFrninOPR_cF4iw&usqp=CAU",
-    },
-    {
-      _id: 7,
-      name: "KEYBOARD",
-      description: "hacker pick",
-      price: 999.99,
-      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhF1MePWubfQaH5IsEoOcJFrninOPR_cF4iw&usqp=CAU",
-    },
-    {
-      _id: 8,
-      name: "KEYBOARD",
-      description: "hacker pick",
-      price: 999.99,
-      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhF1MePWubfQaH5IsEoOcJFrninOPR_cF4iw&usqp=CAU",
-    },
-    {
-      _id: 9,
-      name: "MOUSE",
-      description: "hacker pick",
-      price: 999.99,
-      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhF1MePWubfQaH5IsEoOcJFrninOPR_cF4iw&usqp=CAU",
-    },
-    {
-      _id: 10,
-      name: "MOUSE",
-      description: "hacker pick",
-      price: 999.99,
-      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhF1MePWubfQaH5IsEoOcJFrninOPR_cF4iw&usqp=CAU",
-    },
-    {
-      _id: 11,
-      name: "MOUSE",
-      description: "hacker pick",
-      price: 999.99,
-      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhF1MePWubfQaH5IsEoOcJFrninOPR_cF4iw&usqp=CAU",
-    },
-    {
-      _id: 12,
-      name: "MOUSE",
-      description: "hacker pick",
-      price: 999.99,
-      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhF1MePWubfQaH5IsEoOcJFrninOPR_cF4iw&usqp=CAU",
-    },
-    {
-      _id: 13,
-      name: "DESK",
-      description: "hacker pick",
-      price: 999.99,
-      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhF1MePWubfQaH5IsEoOcJFrninOPR_cF4iw&usqp=CAU",
-    },
-    {
-      _id: 14,
-      name: "DESK",
-      description: "hacker pick",
-      price: 999.99,
-      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhF1MePWubfQaH5IsEoOcJFrninOPR_cF4iw&usqp=CAU",
-    },
-    {
-      _id: 15,
-      name: "DESK",
-      description: "hacker pick",
-      price: 999.99,
-      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhF1MePWubfQaH5IsEoOcJFrninOPR_cF4iw&usqp=CAU",
-    },
-    {
-      _id: 16,
-      name: "DESK",
-      description: "hacker pick",
-      price: 999.99,
-      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhF1MePWubfQaH5IsEoOcJFrninOPR_cF4iw&usqp=CAU",
-    },
-    {
-      _id: 17,
-      name: "CHAIR",
-      description: "hacker pick",
-      price: 999.99,
-      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhF1MePWubfQaH5IsEoOcJFrninOPR_cF4iw&usqp=CAU",
-    },
-    {
-      _id: 18,
-      name: "CHAIR",
-      description: "hacker pick",
-      price: 999.99,
-      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhF1MePWubfQaH5IsEoOcJFrninOPR_cF4iw&usqp=CAU",
-    },
-    {
-      _id: 19,
-      name: "CHAIR",
-      description: "hacker pick",
-      price: 999.99,
-      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhF1MePWubfQaH5IsEoOcJFrninOPR_cF4iw&usqp=CAU",
-    },
-    {
-      _id: 20,
-      name: "CHAIR",
-      description: "hacker pick",
-      price: 999.99,
-      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhF1MePWubfQaH5IsEoOcJFrninOPR_cF4iw&usqp=CAU",
-    },
-
-];
-=======
   const { loading, data } = useQuery(QUERY_ALL_PRODUCTS);
->>>>>>> main
 
   if (loading) {
     return <div>Loading...</div>;
@@ -168,21 +14,22 @@ const products = [
   const products = data?.products || [];
 
   return (
-    <div>
+    <div id='backgroundImageAllProduct'>
       <Container>
-<<<<<<< HEAD
-        <div>
-          <h1 >Laptops</h1>
+        <div id="custom-bg-secondary">
+          <h1 className="display-2 font-weight-bold text-decoration-underline text-white bg-opaque">Chairs</h1>
       <Row>
         {products.slice(0, 4).map((product, i) => (
           <Col xs={12} md={3} key={i}>
+            
             <ProductCard product={product} />
+            
           </Col>
         ))}
       </Row>
       </div>
-      <div>
-          <h1>Keyboards</h1>
+      <div id="custom-bg-secondary">
+          <h1 className="display-2 font-weight-bold text-decoration-underline text-white bg-opaque">Laptops</h1>
       <Row>
         {products.slice(4, 8).map((product, i) => (
           <Col xs={12} md={3} key={i}>
@@ -191,8 +38,8 @@ const products = [
         ))}
       </Row>
       </div>
-      <div>
-          <h1>Mice</h1>
+      <div id="custom-bg-secondary">
+          <h1 className="display-2 font-weight-bold text-decoration-underline text-white bg-opaque">Keyboards</h1>
       <Row>
         {products.slice(8, 12).map((product, i) => (
           <Col xs={12} md={3} key={i}>
@@ -201,8 +48,8 @@ const products = [
         ))}
       </Row>
       </div>
-      <div>
-          <h1>Desks</h1>
+      <div id="custom-bg-secondary">
+          <h1 className="display-2 font-weight-bold text-decoration-underline text-white bg-opaque">Mice</h1>
       <Row>
         {products.slice(12, 16).map((product, i) => (
           <Col xs={12} md={3} key={i}>
@@ -211,8 +58,8 @@ const products = [
         ))}
       </Row>
       </div>
-      <div>
-          <h1>Chairs</h1>
+      <div id="custom-bg-secondary">
+          <h1 className="display-2 font-weight-bold text-decoration-underline  text-white bg-opaque">Desks</h1>
       <Row>
         {products.slice(16, 20).map((product, i) => (
           <Col xs={12} md={3} key={i}>
@@ -221,15 +68,6 @@ const products = [
         ))}
       </Row>
       </div>
-=======
-        <Row>
-          {products.map((product) => (
-            <Col xs={12} md={3} key={product._id}>
-              <ProductCard product={product} />
-            </Col>
-          ))}
-        </Row>
->>>>>>> main
       </Container>
     </div>
   );
