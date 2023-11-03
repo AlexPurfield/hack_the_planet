de4b58fd73d7a44b0fa45186b4034614e2eb9a7f
Alex Purfield <alexpurfield@gmail.com>
2023-11-02 23:27:03 -0600

    trying to get seeds back- added resolvers back

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,11 @@
-const { User, Product, Category, Order } = require('../models');
-const { signToken, AuthenticationError } = require('../utils/auth');
+const { User, Product, Category, Order } = require('./models');
+const { signToken, AuthenticationError } = require('./utils/auth');
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
 
 const resolvers = {
   Query: {
+   
     categories: async () => {
       return await Category.find();
     },
