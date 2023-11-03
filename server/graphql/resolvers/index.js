a4276c9e807719365466abfcbb1e41b47a65479b
Coderinsightmaven <lmarincik@proton.me>
2023-11-02 23:51:40 -0600

    I did stuff yay!

--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -1,29 +1,31 @@
-require('dotenv').config();
-const Stripe = require('stripe');
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY); // Ensure this is set in your .env file
+require("dotenv").config();
+// const Stripe = require("stripe");
+// const stripe = Stripe(process.env.STRIPE_SECRET_KEY); // Ensure this is set in your .env file
 const usersResolvers = require("./users");
+const productsResolvers = require("./products");
 
 module.exports = {
   Query: {
     ...usersResolvers.Query,
+    ...productsResolvers.Query,
   },
   Mutation: {
     ...usersResolvers.Mutation,
     // Add this new mutation
-    createPaymentIntent: async (_, { amount, currency = 'usd' }) => {
-      try {
-        const paymentIntent = await stripe.paymentIntents.create({
-          amount,
-          currency,
-          // Additional options can be added here
-        });
-        return {
-          clientSecret: paymentIntent.client_secret,
-        };
-      } catch (error) {
-        console.error(error);
-        throw new Error("Error creating payment intent.");
-      }
-    },
+    //   createPaymentIntent: async (_, { amount, currency = "usd" }) => {
+    //     try {
+    //       const paymentIntent = await stripe.paymentIntents.create({
+    //         amount,
+    //         currency,
+    //         // Additional options can be added here
+    //       });
+    //       return {
+    //         clientSecret: paymentIntent.client_secret,
+    //       };
+    //     } catch (error) {
+    //       console.error(error);
+    //       throw new Error("Error creating payment intent.");
+    //     }
+    //   },
   },
 };
---
+++ server/graphql/resolvers/products.js
@@ -0,0 +1,25 @@
+const { Product } = require("../../models");
+
+module.exports = {
+  Query: {
+    // Resolver to get all products
+    products: async (_, { category, name }) => {
+      let query = {};
+      if (category) {
+        query.category = category;
+      }
+      if (name) {
+        query.name = { $regex: name, $options: "i" }; // search by name, case insensitive
+      }
+      return await Product.find(query).populate("category");
+    },
+    // Resolver to get a single product by ID
+    product: async (_, { _id }) => {
+      return await Product.findById(_id).populate("category");
+    },
+  },
+  // If you have any mutations related to products, they would go here
+  Mutation: {
+    // Example: addProduct, updateProduct, deleteProduct, etc.
+  },
+};
