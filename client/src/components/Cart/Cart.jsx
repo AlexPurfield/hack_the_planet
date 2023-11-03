3d2303037f579552e067959e8be92ba62f610727
Steven Rodriguez <54337759+StevenRodriguezDev@users.noreply.github.com>
2023-11-03 16:14:49 -0600

    updated file

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -3,7 +3,7 @@ import { loadStripe } from '@stripe/stripe-js';
 import { useLazyQuery } from '@apollo/client';
 import { QUERY_CHECKOUT } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
-import CartItems from '../CartItems';
+// import { CartItem } from '../CartItem';
 import Auth from '../../utils/auth';
 import { useStoreContext } from '../../utils/Global';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
