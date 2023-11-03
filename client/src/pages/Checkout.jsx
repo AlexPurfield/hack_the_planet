525976cf1ecd17c7b18100912c00d91e3f94c14b
Steven Rodriguez <54337759+StevenRodriguezDev@users.noreply.github.com>
2023-11-02 23:17:44 -0600

    updated checkout jsx

---
+++ client/src/pages/Checkout.jsx
@@ -0,0 +1,35 @@
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+
+function Checkout() {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js has not yet loaded.
+      return;
+    }
+
+    const cardElement = elements.getElement(CardElement);
+
+    const { error, token } = await stripe.createToken(cardElement);
+
+    if (error) {
+      console.log('[error]', error);
+    } else {
+      // Send the token to your server to create a charge
+      console.log('[PaymentMethod]', token);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement />
+      <button type="submit" disabled={!stripe}>
+        Pay
+      </button>
+    </form>
+  );
+}
