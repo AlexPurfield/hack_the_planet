e5e4581d9f74b6782ee2205aa9ebf4aa563ad602
AztekAsylum <mateo.rodriguez85@gmail.com>
2023-11-06 11:22:08 -0700

    upadeted login/creat account

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { LOGIN } from "../utils/mutations";
 import Auth from "../utils/auth";
 import Swal from "sweetalert2";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
 
 function Login(props) {
   const [formState, setFormState] = useState({ email: "", password: "" });
@@ -39,105 +40,58 @@ function Login(props) {
   };
 
   return (
-    <div 
-      className="container my-1"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        padding: "10px", // padding at the top
-        width: "100%", // ensure div takes full width
-        
-      }}
-    >
-      <h2 style={{ color: "white", textAlign: "center" }}>LOGIN</h2>
-      <form
-        onSubmit={handleFormSubmit}
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "flex-start",
-          width: "100%", // full width of the container
-          maxWidth: "320px", // max width of the form
-          padding: "1rem",
-          boxSizing: "border-box",
-          backgroundColor: "black",
-        }}
-      >
-        <div
-          className="flex-row space-between my-2"
-          style={{ marginBottom: "1rem", width: "100%" }}
-        >
-          <label
-            htmlFor="email"
-            style={{ marginBottom: "0.5rem", color: "black" }}
-          >
-            Email address:
-          </label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            value={formState.email}
-            onChange={handleChange}
-            style={{ width: "100%", padding: "0.5rem" }}
-          />
-        </div>
-        <div
-          className="flex-row space-between my-2"
-          style={{ marginBottom: "1rem", width: "100%" }}
-        >
-          <label
-            htmlFor="pwd"
-            style={{ marginBottom: "0.5rem", color: "black" }}
-          >
-            Password:
-          </label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            value={formState.password}
-            onChange={handleChange}
-            style={{ width: "100%", padding: "0.5rem" }}
-          />
-        </div >
-        {error && (
-          <div style={{ color: "red", marginBottom: "1rem" }}>
-            <p className="error-text">The provided credentials are incorrect</p>
-          </div>
-        )}
-        <div
-          className="flex-row flex-end"
-          style={{
-            width: "100%",
-            display: "flex",
-            justifyContent: "center", // Centers horizontally in the container
-            alignItems: "center",
-          }}
-        >
-          <button
-            type="submit"
-            style={{
-              padding: "0.5rem 1rem",
-              cursor: "pointer",
-              color: "white",
-            }}
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-      <Link
-        to="/createaccount"
-        style={{ color: "white", padding: "0.5rem 1rem" }}
-      >
-        Don't have an account? Sign up here!
-      </Link>
-    </div>
+    <Container className="mb-5 mt-3">
+      <Row className="text-center">
+        <h1>LOGIN</h1>
+      </Row>
+      <Row>
+        <Col xs={8} className="mx-auto">
+          <Form onSubmit={handleFormSubmit}>
+            <Form.Group className="mb-3" controlId="formGroupEmail">
+              <Form.Label>EMAIL ADDRESS</Form.Label>
+              <Form.Control
+                placeholder="youremail@test.com"
+                name="email"
+                type="email"
+                value={formState.email}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formGroupPassword">
+              <Form.Label>PASSWORD</Form.Label>
+              <Form.Control
+                placeholder="******"
+                name="password"
+                type="password"
+                value={formState.password}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Container className="mx-auto">
+              {error && (
+                <Row style={{ color: "red", marginBottom: "1rem" }}>
+                  <p className="error-text">
+                    The provided credentials are incorrect
+                  </p>
+                </Row>
+              )}
+              <Row className="text-center">
+                <Col>
+                  <Button variant="dark" type="submit">
+                    SUBMIT
+                  </Button>
+                </Col>
+              </Row>
+            </Container>
+          </Form>
+          <Row className="text-center mt-3">
+            <span style={{ color: "whitesmoke" }}>
+              Don't have an account? <Link to="/login"> Sign up here!</Link>
+            </span>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
