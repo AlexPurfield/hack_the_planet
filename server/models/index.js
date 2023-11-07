046f06a188e641fe3af772baa28e85c2b4842501
Steven Rodriguez <54337759+StevenRodriguezDev@users.noreply.github.com>
2023-11-06 21:52:50 -0700

    AINT NOBODY COOL

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,5 @@
 const Product = require("./product");
 const Order = require("./Order");
 const User = require("./User");
-const Category = require("./Category");
+const Category = require("./category");
 module.exports = { User, Product, Order, Category };
